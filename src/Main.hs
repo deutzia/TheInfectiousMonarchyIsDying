e126019321b9dfffb2e872ea9dc25488d8d6ea5c
Iwona Kotlarska <pinus314@gmail.com>
2020-04-13 03:50:15 +0200

    Add main that opens and reads file or reads stdin

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import System.Environment
+import Data.Void
+import Control.Monad
+
+type Parser = Parsec Void String
+
+main = do
+    argv <- getArgs
+    contents <- if null argv then getContents else readFile $ head argv
+    putStrLn contents
